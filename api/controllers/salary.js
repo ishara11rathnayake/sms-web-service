7f30c47e036ae149aaff351409eeabe993fbaeb9
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-12-15 23:43:42 +0530

    get salary feature

--- api/controllers/salary.js
+++ api/controllers/salary.js
@@ -1,4 +1,5 @@
 const Salary = require("../models/salary");
+const Teacher = require("../models/teacher");
 const mongoose = require("mongoose");
 
 exports.salary_add_salary = async (req, res, next) => {
@@ -17,6 +18,28 @@ exports.salary_add_salary = async (req, res, next) => {
 
     res.status(201).json({
       message: "Salary added successfully."
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: error
+    });
+  }
+};
+
+exports.salary_get_salary = async (req, res, next) => {
+  try {
+    const salary = await Salary.find({
+      year: req.query.year,
+      month: req.query.month,
+      user: req.userData.userId
+    });
+
+    const teacherDetails = await Teacher.find({ user: req.userData.userId });
+
+    res.status(200).json({
+      salary: salary[0],
+      teacher: teacherDetails[0]
     });
   } catch (error) {
     console.log(error);
--- api/routes/salary.js
+++ api/routes/salary.js
@@ -6,5 +6,6 @@ const { upload } = require("../helpers/uploadNotes");
 const SalaryController = require("../controllers/salary");
 
 router.post("/", checkAuth, upload, SalaryController.salary_add_salary);
+router.get("/byuser", checkAuth, upload, SalaryController.salary_get_salary);
 
 module.exports = router;
