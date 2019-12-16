eb3ff9309f01e9eb4d0ab8f8d1b00f8ed44d4c2e
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-12-16 10:36:15 +0530

    get student by class

--- api/controllers/student.js
+++ api/controllers/student.js
@@ -194,7 +194,19 @@ exports.students_get_students_byclass = async (req, res, next) => {
 
     res.status(200).json({
       count: students.length,
-      students: students
+      students: students.map(student => {
+        return {
+          studentId: student._id,
+          fullname: student.full_name,
+          nameinitials: student.name_with_initial,
+          gender: student.gender,
+          dob: student.dob,
+          grade: student.grade,
+          class: student.class,
+          admissionnumber: student.admission_number,
+          admissiondate: student.admission_date
+        };
+      })
     });
   } catch (error) {
     console.log(error);
